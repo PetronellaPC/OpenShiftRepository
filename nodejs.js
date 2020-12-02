c8a431548c6dabc592466366f7b80b5a1164f01b
root <root@workstation.lab.example.com>
2020-12-02 12:23:34 -0500

    scripting files added, change to node js app

--- nodejs.js
+++ nodejs.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const os = require('os');
-console.log("Kubia server starting...");
+console.log("Changed server starting.. blah blah.");
 var handler = function(request, response) {  console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
   response.end("You've hit " + os.hostname() + "\n");
